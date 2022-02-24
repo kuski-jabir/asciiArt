6e74ef0a5102b30c141b46d15e6ac7691f647c7c
kuski-jabir <kuskihaji@gmail.com>
2022-02-24 18:20:47 +0300

    completed the ascii art

--- ascii_art.py
+++ ascii_art.py
@@ -1,31 +1,25 @@
 from PIL import Image
+from numpy import asarray, reshape
 
+image = Image.open("kingfisher.jpg")
+if image.width > 200:
+    new_image = image.resize((200, image.height))
+    new_image.save(f"{image}_resized.jpg")
 
-#loading image and printing its dimensions
-im = Image.open("melons.jpg")
-pixel_size = [str(i) for i in im.size]
-print("image successfully loaded !")
-print("Image size : " + " x ".join(pixel_size))
-
-new_image = im.resize((400,200))
-new_image.save('melons-resize.jpg')
-
-#converting pixels into an array of tuples
-#temp = np.asarray(im)
-
-#for i in temp:
-#    pixel_array = [[j[0],j[1],j[2]] for j in i] 
-
-
-#creating a list of brightness of the pixel
-
-
-#mapping the brightness to ascii characters
+pixel_size = [str(i) for i in new_image.size]
+print("new_image successfully loaded")
+print(f" new_image size : " + " X ".join(pixel_size))
 brightness_test = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
-def printAscii():
-    pixel_array = new_image.getdata()
-    brightness_list = [sum(i) // 3 for i in pixel_array]
-    final_image = [brightness_test[int(i // 3.93)]  for i in brightness_list]
-    print("".join(final_image))
-printAscii()
+pixels = new_image.getdata()
+brighness_level = [sum(i) // 3 for i in pixels]
+final_new_image = [str(brightness_test[int(i // 3.93)]) * 2   for i in brighness_level]
+print(len(brighness_level))
+
+result = reshape(asarray(final_new_image), (-1, 200)).tolist()
+
+for i in range(len(result)):
+    for j in range(len(result[1])):
+        print(result[i][j], end="")
+    print()
+    
